5618b93220d6f5781289e45887221f472af8d1a4
danielzm05 <danielzmfcbarcelona@gmail.com>
2024-06-25 09:45:28 -0300

    now its possible edit the codigo property of the Cuentas Table

--- src/components/Modals/ModifyAccountModal.jsx
+++ src/components/Modals/ModifyAccountModal.jsx
@@ -3,11 +3,14 @@ import { Modal } from "../Modal";
 import { useAccounts } from "../../context/AccountContext";
 
 export function ModifyAccountModal({ isOpen, onClose, account }) {
+  const [error, setError] = useState(false);
   const [accountModified, setAccountModified] = useState({
     codigo: "",
     nombre: "",
     tipo: "",
   });
+
+  const { accounts, updateAccount } = useAccounts();
 
   useEffect(() => {
     setAccountModified({
@@ -18,12 +21,20 @@ export function ModifyAccountModal({ isOpen, onClose, account }) {
     });
   }, [account]);
 
+  useEffect(() => {
+    if (accountModified.codigo != account.codigo) {
+      if (accounts.some((acct) => acct.codigo == accountModified.codigo)) {
+        setError(true);
+      }
+    } else {
+      setError(false);
+    }
+  }, [accountModified.codigo]);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setAccountModified({ ...accountModified, [name]: value });
   };
-
-  const { updateAccount } = useAccounts();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -41,6 +52,15 @@ export function ModifyAccountModal({ isOpen, onClose, account }) {
     <Modal isOpen={isOpen} isClose={onClose}>
       <h3>Modificar Cuenta</h3>
       <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="codigo">Código:</label>
+        <input
+          type="number"
+          name="codigo"
+          id="codigo"
+          value={accountModified.codigo}
+          className={`input-data ${error ? "error" : ""}`}
+          onChange={handleChange}
+        />
         <label htmlFor="nombre">Nombre:</label>
         <input
           type="text"
@@ -62,7 +82,14 @@ export function ModifyAccountModal({ isOpen, onClose, account }) {
           <option value="Deudor">Deudor</option>
         </select>
         <div className="buttons-container">
-          <input type="submit" value="Modificar" />
+          <span className="error-message">
+            {error ? "⚠︎ Error: Código ya existente" : ""}
+          </span>
+          <input
+            type="submit"
+            value="Modificar"
+            className={error ? "hide-btn" : ""}
+          />
         </div>
       </form>
     </Modal>
